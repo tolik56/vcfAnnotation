7540ca2edac01787e8aa06bda3107d69813c89e2
Anatoly Ulyanov <anatoly@Pluto.local>
2017-12-17 20:13:50 -0600

    Final fix

--- getExacAnn.sh
+++ getExacAnn.sh
@@ -10,7 +10,7 @@ gawk '/^[^#]/{OFS="-";print $1,$2,$4,$5}' $TMP/$1 > $TMP/queryExac
 echo "allele_fq" > $TMP/alfqExac 
 echo "filter" > $TMP/fltrExac
 
-for i in $(cat queryExac)
+for i in $(cat $TMP/queryExac)
 do
   # echo $i
   wget --quiet --output-document - "http://exac.hms.harvard.edu/rest/variant/variant/$i" | tr , '\n' | gawk -F: '/allele_freq/{print $2;f=1}END{if(f!=1)print "NA"}' >> $TMP/alfqExac
